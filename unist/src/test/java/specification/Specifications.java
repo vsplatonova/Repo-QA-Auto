cc9438bca820b7db1fe521d065c5318afad90118
vs_pl <vs_platonova@outlook.com>
2023-04-17 20:10:26 +0300

    Unistream

---
+++ unist/src/test/java/specification/Specifications.java
@@ -0,0 +1,18 @@
+package specification;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import static io.restassured.RestAssured.requestSpecification;
+
+
+public class Specifications {
+    @BeforeEach
+    void beforeTest() {
+
+        requestSpecification = new RequestSpecBuilder()
+                .log(LogDetail.ALL)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+}
